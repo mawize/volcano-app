e962c90fe3bceeb0c735ce426cdff1b8156443bb
Max <mawize@gmail.com>
2025-05-20 16:00:14 +0200

    Single spinner centered same font size

--- src/features/deviceBLEconnection/LoadingConnection.jsx
+++ src/features/deviceBLEconnection/LoadingConnection.jsx
@@ -3,22 +3,25 @@ import styled from "styled-components";
 import { PrideTextWithDiv } from "../../themes/PrideText";
 
 const Div = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
   background-color: ${(props) => props.theme.backgroundColor};
-  color: ${(props) => props.theme.primaryFontColor};
+  color: ${(props) => props.theme.iconColor};
 `;
 
 export default function Loading() {
   return (
     <Div>
-      <Spinner animation="border" variant="primary" />
-      <Spinner animation="border" variant="secondary" />
-      <Spinner animation="border" variant="success" />
-      <Spinner animation="border" variant="danger" />
-      <Spinner animation="border" variant="warning" />
-      <Spinner animation="border" variant="info" />
-      <Spinner animation="border" variant="light" />
-      <Spinner animation="border" variant="dark" />
-      <PrideTextWithDiv text="Establishing BLE Connection" />
+      <Spinner animation="border" style={{ color: (props) => props.theme.iconColor }} />
+      <PrideTextWithDiv text="Connecting to Volcano ..." />
     </Div>
   );
 }
